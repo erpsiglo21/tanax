1f1cdbdb2921c6834585ee5d1c25a93dffa08f7b
Richard Flores <r.flores.l@gmail.com>
2021-06-23 01:32:53 -0400

    Agrega porcentaje de exigencia

--- tanax_maestros/__manifest__.py
+++ tanax_maestros/__manifest__.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Tanax Maestros',
-    'version': '0.1',
+    'version': '0.2',
     'license': 'AGPL-3',
     'summary': 'Agrega informacion necesaria en los maestros',
     'author': u'ERP Siglo 21',
     'depends': ['base'],
     'data': [
         "security/ir.model.access.csv",
+        "security/security.xml",
+        'views/res_canal_views.xml',
         'views/res_partner_views.xml',
     ],
     'installable': True,
