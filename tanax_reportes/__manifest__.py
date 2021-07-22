e05c9dde85562ddc59d323d481673f9ab1a2f459
Richard Flores <r.flores.l@gmail.com>
2021-07-22 19:17:39 -0400

    [MOD] DTEs con manejo de UM

---
+++ tanax_reportes/__manifest__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Tanax Reportes',
+    'version': '0.1',
+    'license': 'AGPL-3',
+    'summary': 'Reporteria de tanax',
+    'author': u'ERP Siglo 21',
+    'depends': ['l10n_cl_edi'],
+    'data': [
+        'reports/report_invoice.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': False
+}
