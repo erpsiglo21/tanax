29d02f8c5ae7a113c1dd9a0827cd63a5d201c982
Richard Flores <r.flores.l@gmail.com>
2021-08-04 09:47:59 -0400

    [MOD] Guia despacho

--- tanax_reportes/__manifest__.py
+++ tanax_reportes/__manifest__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Tanax Reportes',
-    'version': '0.2',
+    'version': '0.3',
     'license': 'AGPL-3',
     'summary': 'Reporteria de tanax',
     'author': u'ERP Siglo 21',
     'depends': ['sale_management','l10n_cl_edi',],
     'data': [
         'security/ir.model.access.csv',
+        'reports/delivery_guide_document.xml',
         'reports/report_invoice.xml',
         'reports/vtaprod.xml',
     ],
