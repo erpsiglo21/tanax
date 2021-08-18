33bffbe6b8c4cabb8648ae6d6cc96906d95c9d5e
Richard Flores <r.flores.l@gmail.com>
2021-08-18 15:30:20 -0400

    [FIX] Elimina oc_cliente y usa client_order_ref

--- tanax_maestros/models/sale_order.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import api, models, fields, _
-
-class SaleOrder(models.Model):
-    _inherit = "sale.order"
-
-    oc_cliente = fields.Char(string="OC Cliente")
-    
