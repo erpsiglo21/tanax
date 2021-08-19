bdcd133307e8e42b93fc0cf77c10fb87cbd436a2
Richard Flores <r.flores.l@gmail.com>
2021-08-18 21:42:27 -0400

    [FIX] OC cliente

--- tanax_maestros/models/stock_picking.py
+++ tanax_maestros/models/stock_picking.py
@@ -4,6 +4,8 @@ from odoo import api, models, fields, _
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
+    client_order_ref = fields.Char('OC Cliente', compute='_compute_oc_cliente', store=False)
+
     fecha_agendamiento = fields.Datetime(
         'Fecha agendamiento', 
         help="Fecha de agendamiento")
@@ -13,3 +15,9 @@ class StockPicking(models.Model):
     fecha_entrega = fields.Datetime(
         'Fecha real de entrega', 
         help="Fecha en que se realizo la entrega al cliente")
+
+    @api.depends('sale_id')
+    def _compute_oc_cliente(self):
+        for picking in self:
+            if picking.sale_id:
+                picking.client_order_ref = picking.sale_id.client_order_ref
