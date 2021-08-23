f16443b96429b8a26f26ccee75e53f7f2e5db207
Richard Flores <r.flores.l@gmail.com>
2021-08-23 14:36:45 -0400

    [FIX] Agrega factura a vista stock_picking

--- tanax_maestros/models/stock_picking.py
+++ tanax_maestros/models/stock_picking.py
@@ -4,7 +4,8 @@ from odoo import api, models, fields, _
 class StockPicking(models.Model):
     _inherit = "stock.picking"
 
-    client_order_ref = fields.Char('OC Cliente', compute='_compute_oc_cliente', store=False)
+    client_order_ref = fields.Char('OC Cliente', compute='_compute_oc_cliente_factura', store=False)
+    invoice_number = fields.Char('Factura', compute='_compute_oc_cliente_factura', store=False)
 
     fecha_agendamiento = fields.Date(
         'Fecha agendamiento', 
@@ -17,9 +18,12 @@ class StockPicking(models.Model):
         help="Fecha en que se realizo la entrega al cliente")
 
     @api.depends('sale_id')
-    def _compute_oc_cliente(self):
+    def _compute_oc_cliente_factura(self):
         for picking in self:
+            picking.client_order_ref = None
+            picking.invoice_number = None
             if picking.sale_id:
                 picking.client_order_ref = picking.sale_id.client_order_ref
-            else:
-                picking.client_order_ref = None
+                invoices = picking.sale_id.invoice_ids.filtered(lambda inv: inv.state == 'posted' and inv.move_type == 'out_invoice')
+                if invoices:
+                    picking.invoice_number = invoices[0].name
